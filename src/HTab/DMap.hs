d0a68d13f88037588341e9f94780776426e06233
guillaume.hoffmann <guillaume.hoffmann@loria.fr>
2008-10-29 22:23:04 +0000

    some comments in DMap

--- src/HTab/DMap.hs
+++ src/HTab/DMap.hs
@@ -4,13 +4,20 @@ import qualified Data.Map as Map
 import HTab.Formula(BranchingPrefixes, bps_union)
 
 
+{- a DMap , or double map, is a nesting of two Maps -}
 
-flattenDMap :: Map.Map a (Map.Map b c) -> [((a,b),c)]
+type DMap a b c = Map.Map a (Map.Map b c)
+
+flattenDMap :: DMap a b c -> [((a,b),c)]
 flattenDMap m
  = let ambcs = Map.assocs m  in --  [(a,Map.Map b c)]
     concatMap (\(a_,innerM_) ->  map  (\(b_,c_) -> ((a_,b_),c_))  (Map.assocs innerM_  {- [(b,c)] -} )) ambcs
 
-moveInnerDataDMap :: (Ord a, Ord b) => Map.Map a (Map.Map b c) -> a -> a -> (c -> c -> c) -> Map.Map a (Map.Map b c) 
+
+-- provided two keys of the DMap and a merge function, merge the inner maps of
+-- both keys using the merge function when needed for inner values
+-- and delete the first inner map
+moveInnerDataDMap :: (Ord a, Ord b) => DMap a b c -> a -> a -> (c -> c -> c) -> DMap a b c 
 moveInnerDataDMap m origKey destKey innerInnerMergeF
  = result
    where mOrigInnerMap = Map.lookup origKey m
@@ -24,8 +31,8 @@ moveInnerDataDMap m origKey destKey innerInnerMergeF
                                 Map.insert destKey mergedInnerMap prunedM
 
 
--- same, but add dependencies to the copied formulas
-moveInnerDataDMapPlusDeps :: (Ord a, Ord b) => BranchingPrefixes -> Map.Map a (Map.Map b [(BranchingPrefixes,c)]) -> a -> a -> Map.Map a (Map.Map b [(BranchingPrefixes,c)]) 
+-- a specialised version of the previous function, that handles dependencies merging and adding
+moveInnerDataDMapPlusDeps :: (Ord a, Ord b) => BranchingPrefixes -> DMap a b [(BranchingPrefixes,c)] -> a -> a -> DMap a b [(BranchingPrefixes,c)]
 moveInnerDataDMapPlusDeps newDeps m origKey destKey
  = result
    where mOrigInnerMap = Map.lookup origKey m
@@ -46,5 +53,4 @@ moveInnerDataDMapPlusDeps newDeps m origKey destKey
                                    mergedInnerMap       = Map.unionWith innerInnerMergeF origInnerMapPlusDeps destInnerMap
                                in
                                 Map.insert destKey mergedInnerMap prunedM
-
 
