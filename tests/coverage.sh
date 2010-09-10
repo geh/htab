5e33226d3c3ee39e822dc6383ffffaa5cd2f909b
Guillaume Hoffmann <guillaume.hoffmann@loria.fr>
2010-09-10 14:55:33 +0000

    add a hpc based coverage script

---
+++ tests/coverage.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+cd ../src/
+
+rm -rf  htab *.o *.hi HTab/*.o HTab/*.hi *.tix ../html
+ghc --make -fhpc -fglasgow-exts htab.hs
+
+HTAB=./htab
+DATA=../examples/sat/
+
+for i in $DATA/*; do
+  $HTAB                                 -f $i
+  $HTAB -v --stats=10                   -f $i
+  $HTAB -t 2                            -f $i
+  $HTAB -t 2 --lazybranching=False      -f $i
+  $HTAB -t 2 --eager                    -f $i
+  $HTAB -t 2 --unit-prop                -f $i
+  $HTAB -t 2 --no-unit-prop             -f $i
+  $HTAB -t 2 --alltransitive            -f $i
+  $HTAB -t 2 --allreflexive             -f $i
+  $HTAB -t 2 --allsymmetric             -f $i
+  $HTAB -t 2 --allfunctional            -f $i
+  $HTAB -t 2 --allinjective             -f $i
+  $HTAB -t 2 -m mod                     -f $i
+done 
+
+$HTAB --help
+hpc markup htab --destdir=../html
+
+rm -rf htab htab.tix mod *.o *.hi HTab/*.o HTab/*.hi
