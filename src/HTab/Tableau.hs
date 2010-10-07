509c7e43f2dcb3819a7ee4b326f7ae67dfcb4de4
Guillaume Hoffmann <guillaume.hoffmann@loria.fr>
2010-10-07 18:28:54 +0000

    use tableauDown and tableauRight as function names

--- src/HTab/Tableau.hs
+++ src/HTab/Tableau.hs
@@ -17,10 +17,10 @@ data OpenFlag = OPEN Model | CLOSED DependencySet
 type TableauMonad a = StateT Statistics IO a
 
 tableauStart :: Params -> BranchInfo -> TableauMonad OpenFlag
-tableauStart p bi = (configureStats p) >> tableau p 0 bi
+tableauStart p bi = configureStats p >> tableauDown p 0 bi
 
-tableau :: Params -> Depth -> BranchInfo -> TableauMonad OpenFlag
-tableau p depth branchInfo =
+tableauDown :: Params -> Depth -> BranchInfo -> TableauMonad OpenFlag
+tableauDown p depth branchInfo =
       do let verbose = lift . whenLoud . putStrLn
          printOutMetrics
          modify updateStep
@@ -42,18 +42,17 @@ tableau p depth branchInfo =
                       do verbose (">> Rule : " ++ show rule)
                          recordFiredRule $ ruleToId rule
                          case applyRule p rule newBranch newTodo of
-                          [newBi] -> tableau p (depth + 1) newBi
-                          bis     -> chooseBranch p dsEmpty bis (depth + 1)
+                          [newBi] -> tableauDown  p (depth + 1) newBi
+                          bis     -> tableauRight p (depth + 1) bis dsEmpty
 
-
-chooseBranch :: Params -> DependencySet -> [BranchInfo] -> Depth -> TableauMonad OpenFlag
-chooseBranch p currentDepSet (hd:tl) depth =
- do res <- tableau p depth hd
+tableauRight :: Params -> Depth -> [BranchInfo] -> DependencySet -> TableauMonad OpenFlag
+tableauRight p depth (hd:tl) currentDepSet =
+ do res <- tableauDown p depth hd
     case res of
      o@(OPEN _)    -> return o
      CLOSED depSet ->
          if backjumping p && not (dsMember depth depSet)
           then return $ CLOSED depSet
-          else chooseBranch p (dsUnion currentDepSet depSet) tl depth
+          else tableauRight p depth tl (dsUnion currentDepSet depSet)
 
-chooseBranch _ currentDepSet [] _ = return $ CLOSED currentDepSet
+tableauRight _ _ [] currentDepSet = return $ CLOSED currentDepSet
