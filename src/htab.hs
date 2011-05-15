95c6ccab0e175f4372c745ac30cad1ac4aee66dd
Guillaume Hoffmann <guillaumh@gmail.com>
2011-05-15 13:16:44 +0000

    bump to v1.5.6, update mail addr

--- src/htab.hs
+++ src/htab.hs
@@ -47,7 +47,7 @@ runCmdLineVersion =
       else return Nothing
 
 header :: String
-header = unlines ["HTab 1.5.5",
+header = unlines ["HTab 1.5.6",
                   "G. Hoffmann, C. Areces, D.Gorin and J. Heguiabehere. (c) 2002-2011.",
                   "http://code.google.com/p/intohylo/"]
 
