0cdaa94da9cd1999fa8c99057fe8da18cfdae365
Guillaume Hoffmann <guillaumh@gmail.com>
2016-06-29 20:45:40 +0000

    all translations from memory to RC

--- src/HTab/Formula.hs
+++ src/HTab/Formula.hs
@@ -658,64 +658,155 @@ mmd Kn         = 0
 -- ^ translate a memory logic formula into a global sabotage
 --   formula where modalities are R and GSB
 memGSb :: MemFormula -> Formula
-memGSb f = struct (md f) `conj` go f
+memGSb f = struct (mmd f) `conj` go f
  where
    neg_s = Lit (NegLit (P "S"))
-   s = Lit (PosLig (P "S"))
+   s = Lit (PosLit (P "S"))
    nestBox 0 f = f
    nestBox n f = nestBox (n-1) (Box "R" f)
-   struct n = foldr MCon mtop [ nestBox i (neg_s `imp` Dia "R" s) | i <- [0..n]]
+   struct n = foldr conj taut [ nestBox i (neg_s `imp` Dia "R" s) | i <- [0..n]]
 
-   go (Kn)       = Neg (Dia "R" s)
-   go (Re f)     = Dia "GSB" ( (Neg (Dia "R" s)) `conj` go f)
-   go (MDia f)   = Dia "R" (go f)
-   go (MBox f)   = Box "R" (go f)
+   go (Kn)       = neg (Dia "R" s)
+   go (Re f)     = Dia "GSB" ( (neg (Dia "R" s)) `conj` go f)
+   go (MDia f)   = Dia "R" (s `conj` go f)
+   go (MBox f)   = Box "R" (neg_s `imp` go f)
    go (MCon f g) = (go f) `conj` (go g)
    go (MDis f g) = (go f) `disj` (go g)
-   go (MLit l)   = go (Lit l)
-   go (MNeg f)   = go (Neg f)
+   go (MLit l)   = Lit l
+   go (MNeg f)   = neg (go f)
 
 memGSw :: MemFormula -> Formula
-memGSw f = struct (md f) `conj` go f
+memGSw f = struct (mmd f) `conj` go f
  where
    neg_s = Lit (NegLit (P "S"))
-   s = Lit (PosLig (P "S"))
+   s = Lit (PosLit (P "S"))
    nestBox 0 f = f
    nestBox n f = nestBox (n-1) (Box "R" f)
-   struct n = foldr MCon mtop [ nestBox i (neg_s `imp` Dia "R" s) | i <- [0..n]]
+   struct n = foldr conj taut [ nestBox i (neg_s `imp` Dia "R" s) | i <- [0..n]]
 
-   go (Kn)       = Neg (Dia "R" s)
-   go (Re f)     = Dia "GSW" ( (Neg (Dia "R" s)) `conj` go f)
-   go (MDia f)   = Dia "R" (go f)
-   go (MBox f)   = Box "R" (go f)
+   go (Kn)       = neg (Dia "R" s)
+   go (Re f)     = Dia "GSW" ( (neg (Dia "R" s)) `conj` go f)
+   go (MDia f)   = Dia "R" (s `conj` go f)
+   go (MBox f)   = Box "R" (neg_s `imp` go f)
    go (MCon f g) = (go f) `conj` (go g)
    go (MDis f g) = (go f) `disj` (go g)
-   go (MLit l)   = go (Lit l)
-   go (MNeg f)   = go (Neg f)
+   go (MLit l)   = Lit l
+   go (MNeg f)   = neg (go f)
 
 memGBr :: MemFormula -> Formula
-memGBr f = struct (md f) `conj` go f
+memGBr f = struct (mmd f) `conj` go f
  where
    neg_s = Lit (NegLit (P "S"))
-   s = Lit (PosLig (P "S"))
+   s = Lit (PosLit (P "S"))
    nestBox 0 f = f
    nestBox n f = nestBox (n-1) (Box "R" f)
-   struct n = foldr MCon mtop [ nestBox i (neg_s `imp` Dia "R" s) | i <- [0..(n+1)]]
+   struct n = foldr conj taut [ nestBox i (Box "R" neg_s) | i <- [0..(n+1)]]
 
    go (Kn)       = Dia "R" s
    go (Re f)     = Dia "GBR" ( (Dia "R" s) `conj` go f)
-   go (MDia f)   = Dia "R" (go f)
-   go (MBox f)   = Box "R" (go f)
+   go (MDia f)   = Dia "R" (s `conj` go f)
+   go (MBox f)   = Box "R" (neg_s `imp` go f)
    go (MCon f g) = (go f) `conj` (go g)
    go (MDis f g) = (go f) `disj` (go g)
-   go (MLit l)   = go (Lit l)
-   go (MNeg f)   = go (Neg f)
-
-memLSb :: MemFormula -> Formula
-memLSb = undefined
-
-memLBr :: MemFormula -> Formula
-memLBr = undefined
+   go (MLit l)   = Lit l
+   go (MNeg f)   = neg (go f)
 
 memLSw :: MemFormula -> Formula
-memLSw = undefined
+memLSw f = struct `conj` d (go f)
+  where
+   b   = Box "R"
+   bsw = Box "SW"
+   d   = Dia "R"
+   dsw = Dia "SW"
+   neg_s = Lit (NegLit (P "S"))
+   s = Lit (PosLit (P "S"))
+   nestBox 1 f = f
+   nestBox n f = nestBox (n-1) (Box "R" f)
+
+   uniq =   (Dia "R" (s `conj` (Box "R" (neg taut))))
+          `conj` Box "SW" (s `imp` Box "R" (Box "R" neg_s))
+
+   struct = foldr1 conj
+      [ s
+      , Box "R" neg_s
+      , foldr1 conj [nestBox i (neg_s `imp` uniq) | i <- [1..3]]
+      , foldr1 conj [nestBox i (bsw (s `imp` (b( b( b (s `imp` b (neg taut))))))) | i <- [1,2]]
+      , bsw ( bsw ( neg_s `imp` dsw( s `conj` d(d(dsw(s `conj` d(b(neg_s))))))))
+      ]
+
+   go (MDia f)   = Dia "R" (s `conj` go f)
+   go (MBox f)   = Box "R" (neg_s `imp` go f)
+   go (MCon f g) = (go f) `conj` (go g)
+   go (MDis f g) = (go f) `disj` (go g)
+   go (MLit l)   = Lit l
+   go (MNeg f)   = neg (go f)
+   go (Kn)       = neg (d s)
+   go (Re f)     = dsw (s `conj` d (go f))
+
+
+
+-- TODO maybe add neg t to diamond
+memLBr :: MemFormula -> Formula
+memLBr f = struct `conj` dbr(neg_s `conj` t `conj` dbr(neg_s `conj` neg_t `conj` go f))
+  where
+   neg_s = Lit (NegLit (P "S"))
+   s = Lit (PosLit (P "S"))
+
+   neg_t = Lit (NegLit (P "T"))
+   t = Lit (PosLit (P "T"))
+
+   b   = Box "R"
+   bbr = Box "BR"
+
+   d   = Dia "R"
+   dbr = Dia "BR"
+
+   struct = foldr1 conj
+            [ s
+            , b (neg taut)
+            , bbr (s `imp` bbr neg_s)
+            , bbr (neg_s `imp` b neg_s)
+            ]
+
+   go (MDia f)   = Dia "R" (s `conj` go f)
+   go (MBox f)   = Box "R" (neg_s `imp` go f)
+   go (MCon f g) = (go f) `conj` (go g)
+   go (MDis f g) = (go f) `disj` (go g)
+   go (MLit l)   = Lit l
+   go (MNeg f)   = neg (go f)
+   go (Kn)       = d s
+   go (Re f)     = dbr( s `conj` dbr ( (d s) `conj` (go f)))
+
+memLSb :: MemFormula -> Formula
+memLSb f = struct `conj` d (go f)
+  where
+    neg_s = Lit (NegLit (P "S"))
+    s = Lit (PosLit (P "S"))
+
+    d   = Dia "R"
+    dsb = Dia "SB"
+    b   = Box "R"
+    bsb = Box "SB"
+
+    struct = foldr1 conj
+     [ s
+     , b neg_s
+     , bsb ( bsb ( s `imp` b (d s)))
+     ,   b ( bsb ( s `imp` d (b neg_s )))
+     ,   b (   b ( neg_s `imp` d s))
+     ,   b (   b (   b (s `imp` b (d s))))
+     ,   b (   b ( bsb (s `imp` d ( b neg_s))))
+     ,   b ( bsb ( s `imp` bsb( (b neg_s) `imp` b ( b (s `imp` b ( d s))))))
+     ,   b ( bsb ( s `imp`   b( (b neg_s) `imp` b ( b (s `imp` d ( b neg_s))))))
+     ]
+
+    go (MDia f)   = Dia "R" (s `conj` go f)
+    go (MBox f)   = Box "R" (neg_s `imp` go f)
+    go (MCon f g) = (go f) `conj` (go g)
+    go (MDis f g) = (go f) `disj` (go g)
+    go (MLit l)   = Lit l
+    go (MNeg f)   = neg (go f)
+    go (Kn)       = neg (d s)
+    go (Re f)     = dsb( s `conj` dsb ( neg (d s) `conj` go f))
+
+
