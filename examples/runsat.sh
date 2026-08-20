fd5f3fd82dd905807f2bfbc1e88185448e846317
Guillaume Hoffmann <guillaumh@gmail.com>
2026-08-20 10:58:59 -0300

    fix sat/unsat test scripts

--- examples/runsat.sh
+++ examples/runsat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-HTABPATH="../dist/build/htab/htab"    # path to HTab
 UNSATPATH="sat"              # directory where examples are
 
+type htab # print which path we are using
+
 for file in `ls ${UNSATPATH}/*.frm`;
-do echo $file;${HTABPATH} -f $file $1 $2 $3 $4;
+do echo $file;htab -f $file $1 $2 $3 $4;
 done
 
 
--- examples/rununsat.sh
+++ examples/rununsat.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-HTABPATH="../dist/build/htab/htab"    # path to HTab
 UNSATPATH="unsat"            # directory where examples are
 
+type htab # print which path we are using
+
 for file in `ls ${UNSATPATH}/*.frm`;
-do echo $file;${HTABPATH} -f $file $1 $2 $3 $4;
+do echo $file;htab -f $file $1 $2 $3 $4;
 done
 
